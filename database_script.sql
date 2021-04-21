3685158b51ace92ba1c226800d8a8bd5a87676bf
swgarverick <swgarverick@gmail.com>
2021-04-20 19:45:27 -0500

    Rough outline of db

---
+++ database_script.sql
@@ -0,0 +1,27 @@
+CREATE TABLE MarketUser (
+	username varchar(255) NOT NULL,
+	password varchar(255) NOT NULL,
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE Item (
+	ID int NOT NULL,
+	name varchar(255),
+	price float,
+	remaining_quantity int,
+	seller varchar(255),
+	PRIMARY KEY (ID),
+	FOREIGN KEY (seller) REFERENCES MarketUser(username)
+);
+
+CREATE TABLE Purchase (
+	ID int NOT NULL,
+	buyer varchar(255),
+	seller varchar(255),
+	item int,
+	date date,
+	PRIMARY KEY (ID),
+	FOREIGN KEY (buyer) REFERENCES MarketUser(username),
+	FOREIGN KEY (seller) REFERENCES MarketUser(username),
+	FOREIGN KEY (item) REFERENCES Item(ID)
+);
